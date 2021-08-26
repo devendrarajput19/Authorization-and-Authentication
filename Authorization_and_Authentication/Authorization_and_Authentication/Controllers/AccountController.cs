e7b19aab4c2b36653c0939b0397ab3d598cce620
Rajput <devendra.rajput.ext@nokia.com>
2021-08-26 12:25:20 +0530

    part 1

---
+++ Authorization_and_Authentication/Authorization_and_Authentication/Controllers/AccountController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using Authorization_and_Authentication.Models;
+
+namespace Authorization_and_Authentication.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(User user)
+        {
+            using(var context = new EmployeeEnterpriseEntities())
+            {
+                bool isValid = context.Users.Any(x => x.UserName ==  user.UserName && x.Password == user.Password);
+                if(isValid)
+                {
+                    FormsAuthentication.SetAuthCookie(user.UserName, false);  // Set false, if you dont want to save Username
+                    return RedirectToAction("Index", "Employee");
+                }
+
+                ModelState.AddModelError("", "Invalid Username and Password");
+            }
+            return View();
+        }
+
+        // GET: Account
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(User user)
+        {
+            using (var context = new EmployeeEnterpriseEntities())
+            {
+                context.Users.Add(user);
+                context.SaveChanges();
+            }
+            return RedirectToAction("login");
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("login");
+        }
+    }
+}
---
+++ Authorization_and_Authentication/Authorization_and_Authentication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Authorization_and_Authentication.MvcApplication" Language="C#" %>
